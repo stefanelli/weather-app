f961174b7879e305cf87860e6695158e4e06aebe
Sal@Dell <salvatore.stefanelli@gmail.com>
2022-07-29 21:05:37 +0100

    adding first components

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '46ccce1f2fmsh2b32f011c225a2ap1cbe5djsnb2d702270fbc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
